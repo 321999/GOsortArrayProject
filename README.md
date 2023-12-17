cd71f40597e844dea06142874367f02a59e97e46
321999 <kishorejha321999@gmail.com>
2023-12-17 23:51:31 +0530

    integrated the complete project in singlefile

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# GOsortArrayProject
+Package and Imports:
+<hr>
+package main: This is the entry point for your Go program.
+import ...: These lines import necessary packages for HTTP server functionality, JSON encoding, sorting, and time measurement.
+Port Definition:
+
+const port = 8000: Defines the port on which the server will listen for incoming requests.
+Main Function:
+<hr>
+func main() {...}: This function is the main entry point for your program.
+http.HandleFunc(...): Register handler functions for each endpoint (/process-single and /process-concurrent).
+fmt.Printf(...): Prints a message indicating the server is listening on the specified port.
+http.ListenAndServe(...): Starts the server and listens for incoming requests.
+Request and Response Structures:
+
+type request: Defines the structure of the JSON request with a field ToSort containing an array of sub-arrays to be sorted.
+type response: Defines the structure of the JSON response with fields for the sorted arrays and the execution time in nanoseconds.
+Sequential Processing (processSingle):
+<hr>
+var req request: Declares a variable of type request to store the decoded JSON body.
+json.NewDecoder(...).Decode(...): Decodes the request body into the req variable.
+if err != nil {...}: Handles any errors during decoding.
+startTime := time.Now(): Records the start time for performance measurement.
+for _, subArr := range req.ToSort {...}: Iterates through each sub-array in the req.ToSort field.
+sort.Ints(subArr): Sorts the current sub-array in-place using the standard library's sort.Ints function.
+endTime := time.Now(): Records the end time for performance measurement.
+resp := response{...}: Builds a response object with the sorted arrays and calculated execution time.
+json.NewEncoder(...).Encode(resp): Encodes the response object as JSON and writes it to the response body.
+Concurrent Processing (processConcurrent):
+<hr>
+Similar structure to processSingle until the "Collect sorted sub-arrays from channel" section.
+// for i := range req.ToSort {...}: Commented-out code that originally appended received sub-arrays from the channel directly.
+var channelCount = len(req.ToSort): Stores the number of sub-arrays for later loop usage.
+for range req.ToSort {...}: Iterates through each sub-array, utilizing the pre-stored channelCount for efficiency.
+copy(sortedArrays, sortedArrays[:channelCount]): Shifts existing elements within the sortedArrays slice without reallocation.
+sortedArrays = append(sortedArrays, <-sortedChannel): Appends the received sorted sub-array from the channel.
+This alternative approach avoids repeated appending and improves memory efficiency compared to the commented-out loop.<hr>
--- main.go
+++ main.go
@@ -1,31 +1,226 @@
-// main.go
+// // main.go
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"sort"
+// 	"strings"
+// 	"sync"
+// 	"time"
+// )
+
+// func HomeHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Hello, Go Server!")
+// }
+
+// func SortHandler(w http.ResponseWriter, r *http.Request) {
+// 	// Sample array to sort
+// 	arr := []string{"banana", "apple", "orange", "grape"}
+
+// 	// Sort the array
+// 	sort.Strings(arr)
+
+// 	// Display the sorted array
+// 	fmt.Fprint(w, "Sorted Array: "+strings.Join(arr, ", "))
+// }
+
+// // func main() {
+// // 	http.HandleFunc("/", HomeHandler)
+// // 	http.HandleFunc("/sort", SortHandler) // New route for sorting array
+// // 	fmt.Println("Server is running on http://localhost:8000")
+// // 	// creating a serverwhihc will run on 8000
+// // 	http.ListenAndServe(":8000", nil)
+// // }
+// // main.go
+// // package main
+
+// // import (
+// // 	"fmt"
+// // 	"log"
+// // 	"net/http"
+// // 	"time"
+
+// // 	"github.com/gorilla/mux"
+// // 	"github.com/myusername/GOsortArrayProject/handlers"
+// // )
+
+// func main() {
+// 	r := mux.NewRouter()
+
+// 	r.HandleFunc("/process-single", ProcessSingle).Methods("POST")
+// 	r.HandleFunc("/process-concurrent", ProcessConcurrent).Methods("POST")
+
+// 	port := ":8000"
+// 	fmt.Printf("Server is running on http://localhost%s\n", port)
+
+// 	log.Fatal(http.ListenAndServe(port, r))
+// }
+
+// // building logic  for sorting the array in single and concurrent process
+
+// type RequestPayload struct {
+// 	ToSort [][]int `json:"to_sort"`
+// }
+
+// type ResponsePayload struct {
+// 	SortedArrays [][]int `json:"sorted_arrays"`
+// 	TimeNS       int64   `json:"time_ns"`
+// }
+
+// func ProcessSingle(w http.ResponseWriter, r *http.Request) {
+// 	var request RequestPayload
+// 	decodeJSON(w, r, &request)
+
+// 	startTime := time.Now()
+// 	sortedArrays := make([][]int, len(request.ToSort))
+// 	for i, subArray := range request.ToSort {
+// 		sortedArrays[i] = sortSubArray(subArray)
+// 	}
+// 	endTime := time.Now()
+
+// 	sendResponse(w, ResponsePayload{SortedArrays: sortedArrays, TimeNS: endTime.Sub(startTime).Nanoseconds()})
+// }
+
+// func ProcessConcurrent(w http.ResponseWriter, r *http.Request) {
+// 	var request RequestPayload
+// 	decodeJSON(w, r, &request)
+
+// 	startTime := time.Now()
+// 	var wg sync.WaitGroup
+// 	sortedArrays := make([][]int, len(request.ToSort))
+// 	for i, subArray := range request.ToSort {
+// 		wg.Add(1)
+// 		go func(i int, subArray []int) {
+// 			defer wg.Done()
+// 			sortedArrays[i] = sortSubArray(subArray)
+// 		}(i, subArray)
+// 	}
+// 	wg.Wait()
+// 	endTime := time.Now()
+
+// 	sendResponse(w, ResponsePayload{SortedArrays: sortedArrays, TimeNS: endTime.Sub(startTime).Nanoseconds()})
+// }
+
+// func sortSubArray(subArray []int) []int {
+// 	sortedArray := make([]int, len(subArray))
+// 	copy(sortedArray, subArray)
+// 	sort.Ints(sortedArray)
+// 	return sortedArray
+// }
+
+// func decodeJSON(w http.ResponseWriter, r *http.Request, target interface{}) {
+// 	decoder := json.NewDecoder(r.Body)
+// 	if err := decoder.Decode(target); err != nil {
+// 		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+// 		return
+// 	}
+// }
+
+// func sendResponse(w http.ResponseWriter, response ResponsePayload) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.WriteHeader(http.StatusOK)
+
+//		encoder := json.NewEncoder(w)
+//		if err := encoder.Encode(response); err != nil {
+//			http.Error(w, "Error encoding response", http.StatusInternalServerError)
+//			return
+//		}
+//	}
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sort"
-	"strings"
+	"time"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, Go Server!")
-}
-
-func SortHandler(w http.ResponseWriter, r *http.Request) {
-	// Sample array to sort
-	arr := []string{"banana", "apple", "orange", "grape"}
-
-	// Sort the array
-	sort.Strings(arr)
-
-	// Display the sorted array
-	fmt.Fprint(w, "Sorted Array: "+strings.Join(arr, ", "))
-}
+const port = 8000
 
 func main() {
-	http.HandleFunc("/", HomeHandler)
-	http.HandleFunc("/sort", SortHandler) // New route for sorting array
-	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/process-single", processSingle)
+	http.HandleFunc("/process-concurrent", processConcurrent)
+	fmt.Printf("Server listening on port %d...\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+// input and resoponse format
+type request struct {
+	ToSort [][]int `json:"to_sort"`
+}
+
+type response struct {
+	SortedArrays [][]int `json:"sorted_arrays"`
+	TimeNs       int64   `json:"time_ns"`
+}
+
+// sequential processing
+func processSingle(w http.ResponseWriter, r *http.Request) {
+	var req request
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	startTime := time.Now()
+	for _, subArr := range req.ToSort {
+		sort.Ints(subArr)
+	}
+	endTime := time.Now()
+
+	resp := response{
+		SortedArrays: req.ToSort,
+		TimeNs:       endTime.Sub(startTime).Nanoseconds(),
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+// for concurrent processing
+func processConcurrent(w http.ResponseWriter, r *http.Request) {
+	var req request
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	startTime := time.Now()
+
+	// Channel to receive sorted sub-arrays
+	sortedChannel := make(chan []int)
+
+	// Launch goroutines for each sub-array
+	for _, subArr := range req.ToSort {
+		go func(arr []int) {
+			sort.Ints(arr)
+			sortedChannel <- arr
+		}(subArr)
+	}
+
+	// Collect sorted sub-arrays from channel
+	var sortedArrays [][]int
+	// for i := range req.ToSort {
+	// 	sortedArrays = append(sortedArrays, <-sortedChannel)
+	// }
+
+	var channelCount = len(req.ToSort)
+	for range req.ToSort {
+		copy(sortedArrays, sortedArrays[:channelCount])
+		sortedArrays = append(sortedArrays, <-sortedChannel)
+	}
+
+	endTime := time.Now()
+
+	resp := response{
+		SortedArrays: sortedArrays,
+		TimeNs:       endTime.Sub(startTime).Nanoseconds(),
+	}
+
+	json.NewEncoder(w).Encode(resp)
 }
